49d58b458007deabb6675749f6011007d8462229
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-06-25 08:37:09 +0200

    app_linux.go import runtime

--- app_linux.go
+++ app_linux.go
@@ -2,6 +2,10 @@
 
 package main
 
+import (
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
 func registerGlobalHotkeys(a *App) {
 	runtime.EventsEmit(a.ctx, "failed-to-register-global-stop-hotkey", "Global Hotkeys are not implemented on Linux.")
 	return
