065a0fe0455f45ba3413e7931e03e4379470875b
Yules <189644144+yulesxoxo@users.noreply.github.com>
2025-11-05 12:48:08 +0100

    Xiaomi Input device impl (#436)

--- python/adb_auto_player/scripts/testing.py
+++ python/adb_auto_player/scripts/testing.py
@@ -1,5 +1,4 @@
-from adb_auto_player.device.adb import AdbController
-from adb_auto_player.models.geometry import Point
+from adb_auto_player.device.adb import XiaomiInput
 
 if __name__ == "__main__":
-    AdbController().hold(Point(500, 500))
+    XiaomiInput().key_press(XiaomiInput.keycode("W"), 5)
