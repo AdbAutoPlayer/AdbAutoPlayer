bebba59ce39d1256576ad39ae0664c79d8d6c3ed
Yules <189644144+yulesxoxo@users.noreply.github.com>
2025-11-09 05:13:19 +0100

    AFKJ: Stage rewards tap cancel when receiving equipment chest (#439)
    * AFKJ: Stage rewards tap cancel when chest is received

--- python/adb_auto_player/games/afk_journey/custom_routine/claim_afk_rewards.py
+++ python/adb_auto_player/games/afk_journey/custom_routine/claim_afk_rewards.py
@@ -24,10 +24,11 @@ class ClaimAFKRewards(AFKJourneyBase):
         sleep(1)
         # Make sure the Popup doesn't block collecting AFK Rewards
         self.tap(Point(x=550, y=1080), log=False)
-        if self.settings.claim_afk_rewards:
-            for _ in range(3):
+        if self.settings.claim_afk_rewards.claim_stage_rewards:
+            for _ in range(5):
                 self.tap(Point(x=770, y=500), log=False)
                 self.tap(Point(x=770, y=500), log=False)
                 sleep(1)
-            sleep(1)
+            if cancel := self.game_find_template_match("cancel.png"):
+                self.tap(cancel)
         logging.info("AFK Rewards claimed.")
