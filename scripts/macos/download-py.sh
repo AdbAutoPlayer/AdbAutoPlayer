859be7d25130ac65bd24c6a50e352a379abffed0
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-11-22 14:17:36 +0100

    Python 3.14

--- scripts/macos/download-py.sh
+++ scripts/macos/download-py.sh
@@ -2,7 +2,7 @@
 
 ### Argument ###
 
-PYTHON_VERSION="3.13.9"  # update these by yourself
+PYTHON_VERSION="3.14.0"  # update these by yourself
 TAG="20251031"  # update these by yourself
 TARGET="aarch64-apple-darwin"
 
