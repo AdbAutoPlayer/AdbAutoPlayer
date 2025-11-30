0e2100aa92a5f078b1b26cd84393ec915581480b
Yules <189644144+yulesxoxo@users.noreply.github.com>
2025-11-30 18:47:38 +0100

    Tauri Plugin: Single Instance (#483)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -33,6 +33,12 @@ pub mod ext_mod {
             |_args, _kwargs| Ok(tauri_generate_context()),
             |_args, _kwargs| {
                 let builder = tauri::Builder::default()
+                    .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+                        let _ = app
+                            .get_webview_window("main")
+                            .expect("no main window")
+                            .set_focus();
+                    }))
                     .plugin(tauri_plugin_updater::Builder::new().build())
                     .plugin(tauri_plugin_opener::init())
                     .invoke_handler(tauri::generate_handler![
