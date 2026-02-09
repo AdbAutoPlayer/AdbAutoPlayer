3791de24fb43eec1b82ed4be6375eb3f75f5f832
Yules <189644144+yulesxoxo@users.noreply.github.com>
2026-02-09 03:53:52 +0100

    BS AT Translated Set 2 keyboard: Roughly 40% faster than adb shell input keyevent (#518)

--- src-tauri/src-python/adb_auto_player/device/adb/__init__.py
+++ src-tauri/src-python/adb_auto_player/device/adb/__init__.py
@@ -1,6 +1,7 @@
 from .adb_client import AdbClientHelper
 from .adb_controller import AdbController
 from .adb_input_device import InputDevice
+from .blue_stacks_at_translated_set_2_keyboard import ATTranslatedSet2Keyboard
 from .blue_stacks_virtual_gamepad import BlueStacksVirtualGamepad
 from .blue_stacks_virtual_touch import BlueStacksVirtualTouch
 from .device_stream import DeviceStream, StreamingNotSupportedError
@@ -8,6 +9,7 @@ from .xiaomi_input import XiaomiInput
 from .xiaomi_joystick import XiaomiJoystick
 
 __all__ = [
+    "ATTranslatedSet2Keyboard",
     "AdbClientHelper",
     "AdbController",
     "BlueStacksVirtualGamepad",
