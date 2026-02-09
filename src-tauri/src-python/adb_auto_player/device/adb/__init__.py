e0a859d8b6087a8e86095451154ae366e3699900
Yules <189644144+yulesxoxo@users.noreply.github.com>
2026-02-09 03:09:55 +0100

    BS Virtual Touch: Implement Tap via VT roughly 40% faster taps than shell input tap (#516)

--- src-tauri/src-python/adb_auto_player/device/adb/__init__.py
+++ src-tauri/src-python/adb_auto_player/device/adb/__init__.py
@@ -2,6 +2,7 @@ from .adb_client import AdbClientHelper
 from .adb_controller import AdbController
 from .adb_input_device import InputDevice
 from .blue_stacks_virtual_gamepad import BlueStacksVirtualGamepad
+from .blue_stacks_virtual_touch import BlueStacksVirtualTouch
 from .device_stream import DeviceStream, StreamingNotSupportedError
 from .xiaomi_input import XiaomiInput
 from .xiaomi_joystick import XiaomiJoystick
@@ -10,6 +11,7 @@ __all__ = [
     "AdbClientHelper",
     "AdbController",
     "BlueStacksVirtualGamepad",
+    "BlueStacksVirtualTouch",
     "DeviceStream",
     "InputDevice",
     "StreamingNotSupportedError",
